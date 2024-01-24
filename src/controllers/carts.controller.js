d674bd72e78d9ae2e4407018016a5e831e763773
Jason Andres López <j4ckson95@gmail.com>
2024-01-24 16:52:29 -0500

    Adelanto oficina terminado DAO, falta testing (#1)

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -1,8 +1,10 @@
 import { cartsService } from "../services/main.js"
 
 export const createCart = async (req, res) => {
-    const result = await cartsService.createCart()
-    res.json({ status: "Success", payload: result })
+    try {
+        const result = await cartsService.createCart()
+        res.json({ status: "Success", payload: result })
+    } catch (error) { res.json({ status: "Error", message: error.message }) }
 }
 export const addProductToCart = async (req, res) => {
     const { cid, pid } = req.params
@@ -15,9 +17,9 @@ export const getCartById = async (req, res) => {
     const result = await cartsService.getCartById(cid)
     res.json({ status: "Success", payload: result })
 }
-export const deleteProduct = async (req, res) => {
+export const deleteProducts = async (req, res) => {
     const { cid, pid } = req.params
-    const result = await cartsService.deleteProductOfCart(cid, pid)
+    const result = await cartsService.deleteProductsOfCart(cid, pid)
     res.json({ status: "Success", payload: result })
 }
 export const deleteCart = async (req, res) => {
--- src/routes/carts.route.js
+++ src/routes/carts.route.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { createCart, addProductToCart, getCartById, deleteProduct, deleteCart } from "../controllers/carts.controller.js";
+import { createCart, addProductToCart, getCartById, deleteProducts, deleteCart } from "../controllers/carts.controller.js";
 
 const router = Router()
 
 router.get("/:cid", getCartById)
 router.post("/", createCart)
 router.put("/:cid/product/:pid", addProductToCart)
-router.delete("/:cid/product/:pid", deleteProduct)
+router.delete("/:cid/product/:pid", deleteProducts)
 router.delete("/:cid", deleteCart)
 
 export default router
