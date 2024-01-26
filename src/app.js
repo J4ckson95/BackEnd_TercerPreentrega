16899566bab681b171bf0168307361ae8be703b8
Jason Andres López <j4ckson95@gmail.com>
2024-01-26 16:48:35 -0500

    Adelanto oficina (#2)

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import handlebars from "express-handlebars"
 import productsRouter from "./routes/products.route.js"
 import cartsRouter from "./routes/carts.route.js"
 import userRouter from "./routes/users.route.js"
+import viewsRouter from "./routes/views.route.js"
 import config from "./config/config.js"
 import __dirname from "./utils.js"
 
@@ -17,5 +18,7 @@ app.set("view engine", "handlebars")
 app.use("/api/products", productsRouter)
 app.use("/api/carts", cartsRouter)
 app.use("/api/users", userRouter)
+
+app.use("LibrosAlClic", viewsRouter)
 
 app.listen(config.PORT, () => console.log("Running Server ..."))
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,15 @@
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import config from "./config/config.js"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-export default __dirname 
+export default __dirname
+/*---------------------------------*/
+export const generateHas = (password) => {
+    return bcrypt.hashSync(password, bcrypt.genSalt(10))
+}
+export const validateHas = (user, password) => {
+    return bcrypt.compareSync(password, user.password)
+}
